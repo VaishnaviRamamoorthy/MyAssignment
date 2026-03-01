0e2be83a1e13112d1d56d17c842cfbd35dfe9b8a
VaishnaviRamamoorthy <vysh.ram@gmail.com>
2026-03-01 12:31:17 +0000

    homeAssginmentwith Let

---
+++ Day1/HAWithLet.js
@@ -0,0 +1,12 @@
+
+let username= "John" //String
+let organization="Testleaf" //String
+let contactNum=12334545 //Number
+let knowsAutomation=true //Boolean
+let usesPlaywright //no assignment
+
+console.log(typeof(username))
+console.log(typeof(organization))
+console.log(typeof(contactNum))
+console.log(typeof(knowsAutomation))
+console.log(typeof(usesPlaywright))

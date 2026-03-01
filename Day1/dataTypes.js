3195f0cf4d28ec6d31cb42ad7ad007c35a196b61
VaishnaviRamamoorthy <vysh.ram@gmail.com>
2026-03-01 12:22:54 +0000

    Day1_DataTypes

---
+++ Day1/dataTypes.js
@@ -0,0 +1,34 @@
+//Number
+var mobileNo=1239879871
+console.log(mobileNo)
+console.log(typeof mobileNo);
+
+//Undefined
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright);
+
+//Boolean
+var isAutomation=true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+//String
+var firstName="Vaishnavi"
+console.log(firstName);
+console.log(typeof firstName)
+
+//Null
+var companyName=null;
+console.log(companyName);
+console.log(typeof companyName);
+
+//BigInt
+var y=89789689698798790n
+console.log(y);
+console.log(typeof y);
+
+
+
+
+
